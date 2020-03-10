fb957c36764daeffb46ff243abd0a77e5968b932
rlynjb <rlynjb@users.noreply.github.com>
2020-03-10 11:29:34 -0700

    try inline if condition in template

--- src/Card.js
+++ src/Card.js
@@ -47,7 +47,7 @@ const CityCard = ({ dataObj }) => {
             </Grid>
 
 
-            { if (data.Obj.day) { }
+            { data.Obj.day &&
             <Grid item xs={12} md={6}>
               <Box mb={2}>
                 <Typography variant="h6">
@@ -73,7 +73,7 @@ const CityCard = ({ dataObj }) => {
                 </Grid>
               </Box>
             </Grid>
-            { } }
+            }
 
 
             <Grid item xs={12} md={6}>
